c0706daa1f36b778d006abb6d69a2d6e3ce4d9db
Putri Mila <putrimiila.fx@gmail.com>
2024-09-25 16:44:10 +0800

    1st my Laravel project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Ini halaman about</h3>
+    <p>Penulis: {{ $nama }}</p>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Putri Mila','title' => 'About']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
+
+// Buat 2 rute baru
+// 1 . /blog
+// 2 buah artikel, judul dan isi
+// 2. /contact
+// email / social media
