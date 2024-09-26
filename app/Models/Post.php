9aa1d08e2bd42987637e2f5c978c92fa61bb385a
Putri Mila <putrimiila.fx@gmail.com>
2024-09-26 20:22:35 +0800

    1st my Laravel project

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,44 +1,12 @@
-<?php 
+<?php
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Putri Mila',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus adipisci incidunt corrupti dolor consequatur, fuga omnis dolore sequi neque voluptatibus aperiam quod, ea harum, voluptas doloribus? Libero quisquam atque sint.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Putri Mila',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero quisquam laboriosam esse sequi nemo? Id inventore et, maxime temporibus quae quo laborum dolor, magnam, voluptatibus aliquid ipsa ratione voluptate eos.'
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        // return Arr::first(static::all(), function($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // }); 
-
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug); 
-
-        if(! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
-
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
