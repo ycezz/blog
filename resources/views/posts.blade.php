67803ad7a3abd6003dea23316c6f353da72e041a
Putri Mila <putrimiila.fx@gmail.com>
2024-09-27 10:12:24 +0800

    1st my Laravel project

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,11 @@
       <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
     </a>
     <div class="text-base text-gray-500">
-      <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
+      {{-- By --}}
+      <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> 
+      {{-- in --}}
+      <a href="#" class="hover:underline">Web Programming</a> 
+      | {{ $post->created_at->diffForHumans() }}
     </div>
     <p class="my-4 font-light">
       {{ Str::limit($post['body'], 150) }}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -16,14 +17,19 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('posts/{post:slug}', function (Post $post) {
-
+Route::get('/posts/{post:slug}', function (Post $post) {
         return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user}', function (User $user) {
+    return view('posts', ['title' => 'Articles by' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
+
+
 
 // Buat 2 rute baru
 // 1 . /blog
